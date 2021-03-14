bca60536fce8ab9a19e162735e84e74496033504
IT19180380 <68540529+IT19180380@users.noreply.github.com>
2021-03-14 20:44:12 +0530

    Connect to sqlserver and Check connection

---
+++ Member2/src/DBConnection/SqlServerConnection.java
@@ -0,0 +1,35 @@
+package DBConnection;
+
+import java.sql.*;
+import javax.swing.*;
+public class SqlServerConnection {
+	
+	
+	private static String url = "jdbc:sqlserver://ttmanagement.database.windows.net;databaseName=TTManagement";
+	private static String username = "ITPM";
+	private static String password = "GProject@20";
+	static //private static Connection con;
+	
+	
+	
+	
+	Connection conn=null;
+	public static Connection dbConnecter()
+	{
+		
+		try {
+			
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conn = DriverManager.getConnection(url,username,password);
+			JOptionPane.showMessageDialog(null,"Connection Successfull !");
+			return conn;
+			
+			
+			
+		}catch(Exception e){
+			JOptionPane.showMessageDialog(null,e);
+			return null;
+		}
+			
+	}
+}
