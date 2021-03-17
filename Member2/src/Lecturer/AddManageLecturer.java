320cd3010a5be6f7dc287d261694c9237f55977c
IT19180380 <68540529+IT19180380@users.noreply.github.com>
2021-03-17 09:25:09 +0530

    Add and Manage Lecturer Details-Update 6

--- Member2/src/Lecturer/AddManageLecturer.java
+++ Member2/src/Lecturer/AddManageLecturer.java
@@ -17,6 +17,7 @@ import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JToggleButton;
+import javax.swing.JTabbedPane;
 
 public class AddManageLecturer extends JFrame {
 
@@ -35,6 +36,18 @@ public class AddManageLecturer extends JFrame {
 	private JTextField txtFriday;
 	private JTextField txtSatarday;
 	private JTextField txtSunday;
+	private JTextField txtLecturerID2;
+	private JTextField txtRank2;
+	private JTextField txtLecturerName2;
+	private JTextField txtLecturerName3;
+	private JTextField txtLecturerID3;
+	private JTextField txtMonday3;
+	private JTextField txtTuesday3;
+	private JTextField txtWednesday3;
+	private JTextField txtThursday3;
+	private JTextField txtFriday3;
+	private JTextField txtSatarday3;
+	private JTextField txtSunday3;
 
 	/**
 	 * Launch the application.
@@ -244,17 +257,224 @@ public class AddManageLecturer extends JFrame {
 		LeclayeredPane.add(panel2, "name_74288783095000");
 		panel2.setLayout(null);
 		
-		JLabel lblNewLabel_1 = new JLabel("pannel2");
-		lblNewLabel_1.setBounds(208, 260, 45, 13);
-		panel2.add(lblNewLabel_1);
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_2.setBounds(10, 45, 463, 521);
+		panel2.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Lecturer Name :");
+		lblNewLabel_1.setBounds(36, 86, 109, 13);
+		panel_2.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_18 = new JLabel("Faculty  :");
+		lblNewLabel_18.setBounds(36, 125, 109, 13);
+		panel_2.add(lblNewLabel_18);
+		
+		JLabel lblNewLabel_19 = new JLabel("Department  :");
+		lblNewLabel_19.setBounds(36, 170, 109, 13);
+		panel_2.add(lblNewLabel_19);
+		
+		JLabel lblNewLabel_20 = new JLabel("Campus  :");
+		lblNewLabel_20.setBounds(36, 214, 109, 13);
+		panel_2.add(lblNewLabel_20);
+		
+		JLabel lblNewLabel_21 = new JLabel("Building  :");
+		lblNewLabel_21.setBounds(36, 255, 109, 13);
+		panel_2.add(lblNewLabel_21);
+		
+		JLabel lblNewLabel_22 = new JLabel("Lecturer ID  :");
+		lblNewLabel_22.setBounds(36, 295, 109, 13);
+		panel_2.add(lblNewLabel_22);
+		
+		JLabel lblNewLabel_23 = new JLabel("Level  :");
+		lblNewLabel_23.setBounds(36, 338, 109, 13);
+		panel_2.add(lblNewLabel_23);
+		
+		JLabel lblNewLabel_24 = new JLabel("Rank  :");
+		lblNewLabel_24.setBounds(36, 428, 109, 13);
+		panel_2.add(lblNewLabel_24);
+		
+		txtLecturerName2 = new JTextField();
+		txtLecturerName2.setBounds(155, 83, 164, 19);
+		panel_2.add(txtLecturerName2);
+		txtLecturerName2.setColumns(10);
+		
+		txtLecturerID2 = new JTextField();
+		txtLecturerID2.setBounds(155, 292, 164, 19);
+		panel_2.add(txtLecturerID2);
+		txtLecturerID2.setColumns(10);
+		
+		txtRank2 = new JTextField();
+		txtRank2.setBounds(155, 425, 164, 19);
+		panel_2.add(txtRank2);
+		txtRank2.setColumns(10);
+		
+		JButton btnNewButton_9 = new JButton("Generate ID");
+		btnNewButton_9.setBounds(115, 379, 133, 21);
+		panel_2.add(btnNewButton_9);
+		
+		JComboBox txtFaculty2 = new JComboBox();
+		txtFaculty2.setBounds(152, 121, 167, 21);
+		panel_2.add(txtFaculty2);
+		
+		JComboBox txtDepartment2 = new JComboBox();
+		txtDepartment2.setBounds(152, 166, 167, 21);
+		panel_2.add(txtDepartment2);
+		
+		JComboBox txtCampus2 = new JComboBox();
+		txtCampus2.setBounds(152, 210, 167, 21);
+		panel_2.add(txtCampus2);
+		
+		JComboBox txtBuilding2 = new JComboBox();
+		txtBuilding2.setBounds(152, 251, 167, 21);
+		panel_2.add(txtBuilding2);
+		
+		JComboBox txtLevel2 = new JComboBox();
+		txtLevel2.setBounds(155, 334, 164, 21);
+		panel_2.add(txtLevel2);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_3.setBounds(493, 49, 590, 517);
+		panel2.add(panel_3);
+		panel_3.setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(10, 108, 570, 362);
+		panel_3.add(tabbedPane);
+		
+		JButton btnNewButton_6 = new JButton("Update");
+		btnNewButton_6.setBounds(1111, 172, 109, 39);
+		panel2.add(btnNewButton_6);
+		
+		JButton btnNewButton_7 = new JButton("Delete");
+		btnNewButton_7.setBounds(1111, 221, 109, 39);
+		panel2.add(btnNewButton_7);
+		
+		JButton btnNewButton_8 = new JButton("Clear");
+		btnNewButton_8.setBounds(1111, 270, 109, 39);
+		panel2.add(btnNewButton_8);
 		
 		panel3 = new JPanel();
+		panel3.setForeground(Color.BLACK);
 		LeclayeredPane.add(panel3, "name_74339045343800");
 		panel3.setLayout(null);
 		
-		JLabel lblNewLabel_2 = new JLabel("pannel 3");
-		lblNewLabel_2.setBounds(199, 234, 45, 13);
-		panel3.add(lblNewLabel_2);
+		JPanel panel_4 = new JPanel();
+		panel_4.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_4.setBounds(62, 42, 414, 252);
+		panel3.add(panel_4);
+		panel_4.setLayout(null);
+		
+		JLabel lblNewLabel_2 = new JLabel("Lecturer Name:");
+		lblNewLabel_2.setBounds(63, 81, 116, 13);
+		panel_4.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_25 = new JLabel("Lecturer ID :");
+		lblNewLabel_25.setBounds(63, 129, 116, 13);
+		panel_4.add(lblNewLabel_25);
+		
+		txtLecturerName3 = new JTextField();
+		txtLecturerName3.setBounds(208, 78, 96, 19);
+		panel_4.add(txtLecturerName3);
+		txtLecturerName3.setColumns(10);
+		
+		txtLecturerID3 = new JTextField();
+		txtLecturerID3.setBounds(208, 126, 96, 19);
+		panel_4.add(txtLecturerID3);
+		txtLecturerID3.setColumns(10);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_5.setBounds(582, 42, 431, 252);
+		panel3.add(panel_5);
+		panel_5.setLayout(null);
+		
+		JLabel lblNewLabel_26 = new JLabel("Monday  :");
+		lblNewLabel_26.setBounds(30, 61, 110, 13);
+		panel_5.add(lblNewLabel_26);
+		
+		JLabel lblNewLabel_27 = new JLabel("Tuesday  :");
+		lblNewLabel_27.setBounds(30, 84, 110, 13);
+		panel_5.add(lblNewLabel_27);
+		
+		JLabel lblNewLabel_28 = new JLabel("Wednesday  :");
+		lblNewLabel_28.setBounds(30, 107, 110, 13);
+		panel_5.add(lblNewLabel_28);
+		
+		JLabel lblNewLabel_29 = new JLabel("Thursday  :");
+		lblNewLabel_29.setBounds(30, 130, 110, 13);
+		panel_5.add(lblNewLabel_29);
+		
+		JLabel lblNewLabel_30 = new JLabel("Friday  :");
+		lblNewLabel_30.setBounds(30, 153, 110, 13);
+		panel_5.add(lblNewLabel_30);
+		
+		JLabel lblNewLabel_31 = new JLabel("Satarday  :");
+		lblNewLabel_31.setBounds(30, 176, 110, 13);
+		panel_5.add(lblNewLabel_31);
+		
+		JLabel lblNewLabel_32 = new JLabel("Sunday  :");
+		lblNewLabel_32.setBounds(30, 199, 110, 13);
+		panel_5.add(lblNewLabel_32);
+		
+		txtMonday3 = new JTextField();
+		txtMonday3.setBounds(126, 58, 185, 19);
+		panel_5.add(txtMonday3);
+		txtMonday3.setColumns(10);
+		
+		txtTuesday3 = new JTextField();
+		txtTuesday3.setBounds(126, 84, 185, 19);
+		panel_5.add(txtTuesday3);
+		txtTuesday3.setColumns(10);
+		
+		txtWednesday3 = new JTextField();
+		txtWednesday3.setBounds(126, 107, 185, 19);
+		panel_5.add(txtWednesday3);
+		txtWednesday3.setColumns(10);
+		
+		txtThursday3 = new JTextField();
+		txtThursday3.setBounds(126, 130, 185, 19);
+		panel_5.add(txtThursday3);
+		txtThursday3.setColumns(10);
+		
+		txtFriday3 = new JTextField();
+		txtFriday3.setBounds(126, 153, 185, 19);
+		panel_5.add(txtFriday3);
+		txtFriday3.setColumns(10);
+		
+		txtSatarday3 = new JTextField();
+		txtSatarday3.setBounds(126, 176, 185, 19);
+		panel_5.add(txtSatarday3);
+		txtSatarday3.setColumns(10);
+		
+		txtSunday3 = new JTextField();
+		txtSunday3.setBounds(126, 199, 185, 19);
+		panel_5.add(txtSunday3);
+		txtSunday3.setColumns(10);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_6.setBounds(245, 304, 692, 262);
+		panel3.add(panel_6);
+		panel_6.setLayout(null);
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane_1.setBounds(10, 10, 672, 242);
+		panel_6.add(tabbedPane_1);
+		
+		JButton btnNewButton_10 = new JButton("Update");
+		btnNewButton_10.setBounds(1073, 88, 85, 21);
+		panel3.add(btnNewButton_10);
+		
+		JButton btnNewButton_11 = new JButton("Delete");
+		btnNewButton_11.setBounds(1073, 119, 85, 21);
+		panel3.add(btnNewButton_11);
+		
+		JButton btnNewButton_12 = new JButton("Clear");
+		btnNewButton_12.setBounds(1073, 150, 85, 21);
+		panel3.add(btnNewButton_12);
 		
 		JButton btnNewButton = new JButton("ADD");
 		btnNewButton.addActionListener(new ActionListener() {
