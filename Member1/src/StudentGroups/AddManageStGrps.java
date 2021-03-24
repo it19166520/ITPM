24947e657e3eedd8388b1b5c2418c1fd6444be42
SurekhaChandrarathna <surekhamaduhansi@gmail.com>
2021-03-24 13:16:09 +0530

    Finished design of Add Group Form

--- Member1/src/StudentGroups/AddManageStGrps.java
+++ Member1/src/StudentGroups/AddManageStGrps.java
@@ -17,6 +17,9 @@ import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
 
 public class AddManageStGrps extends JFrame {
 	
@@ -31,6 +34,23 @@ public class AddManageStGrps extends JFrame {
 	private JButton btnAddStGroup_1;
 	private JButton btnManageStGroup_1;
 	private JPanel AddFormPanel;
+	private JLabel lblAcademicYrandsemester;
+	private JLabel lblProgram;
+	private JLabel lblGroupNumber;
+	private JLabel lblSubGroupNumber;
+	private JLabel lblGroupId;
+	private JLabel lblSubGroupId;
+	private JComboBox AcademicYrSemList;
+	private JComboBox ProgramListCB;
+	private JTextField txtAddNewStudent;
+	private JSpinner GroupNoSpinner;
+	private JSpinner SubGroupNoSpinner;
+	private JTextField GrpID;
+	private JTextField SubGrpID;
+	private JButton btnClearAdd;
+	private JButton btnSaveAdd;
+	private JButton btnGenerateGID;
+	private JButton btnGenerateSubGID;
 
 	/**
 	 * Launch the application.
@@ -95,6 +115,124 @@ public class AddManageStGrps extends JFrame {
 		AddFormPanel.setBounds(115, 11, 990, 472);
 		AddStudentGrpPanel_1.add(AddFormPanel);
 		AddFormPanel.setLayout(null);
+		
+		lblAcademicYrandsemester = new JLabel("Academic year and semester :");
+		lblAcademicYrandsemester.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 14));
+		lblAcademicYrandsemester.setBounds(61, 107, 209, 24);
+		AddFormPanel.add(lblAcademicYrandsemester);
+		
+		lblProgram = new JLabel("Program :");
+		lblProgram.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 14));
+		lblProgram.setBounds(61, 168, 209, 24);
+		AddFormPanel.add(lblProgram);
+		
+		lblGroupNumber = new JLabel("Group Number :");
+		lblGroupNumber.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 14));
+		lblGroupNumber.setBounds(61, 234, 209, 24);
+		AddFormPanel.add(lblGroupNumber);
+		
+		lblSubGroupNumber = new JLabel("Sub Group Number :");
+		lblSubGroupNumber.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 14));
+		lblSubGroupNumber.setBounds(61, 295, 209, 24);
+		AddFormPanel.add(lblSubGroupNumber);
+		
+		lblGroupId = new JLabel("Group ID :");
+		lblGroupId.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 14));
+		lblGroupId.setBounds(662, 134, 209, 24);
+		AddFormPanel.add(lblGroupId);
+		
+		lblSubGroupId = new JLabel("Sub Group ID:");
+		lblSubGroupId.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 14));
+		lblSubGroupId.setBounds(662, 295, 209, 24);
+		AddFormPanel.add(lblSubGroupId);
+		
+		String[] AYSList = {"Y1S1","Y1S2","Y2S1","Y2S2","Y3S1","Y3S2","Y4S1","Y4S2"};
+		AcademicYrSemList = new JComboBox(AYSList);
+		AcademicYrSemList.setSelectedIndex(-1);
+		AcademicYrSemList.setBackground(Color.WHITE);
+		AcademicYrSemList.setBounds(280, 107, 222, 29);
+		AddFormPanel.add(AcademicYrSemList);
+		
+		
+		String[] PRList = {"IT" , "SE" , "DS", "CSE","IE" ,"BM"};
+		ProgramListCB = new JComboBox();
+		ProgramListCB.setSelectedIndex(-1);
+		ProgramListCB.setBackground(Color.WHITE);
+		ProgramListCB.setBounds(280, 168, 222, 29);
+		AddFormPanel.add(ProgramListCB);
+		
+		txtAddNewStudent = new JTextField();
+		txtAddNewStudent.setBounds(349, 11, 279, 33);
+		AddFormPanel.add(txtAddNewStudent);
+		txtAddNewStudent.setBackground(new Color(228, 241, 254));
+		txtAddNewStudent.setDisabledTextColor(Color.WHITE);
+		txtAddNewStudent.setEditable(false);
+		txtAddNewStudent.setBorder(null);
+		txtAddNewStudent.setFocusTraversalKeysEnabled(false);
+		txtAddNewStudent.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 18));
+		txtAddNewStudent.setText("Add New Student Group");
+		txtAddNewStudent.setColumns(10);
+		
+		GroupNoSpinner = new JSpinner();
+		GroupNoSpinner.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 13));
+		GroupNoSpinner.setBounds(280, 234, 222, 29);
+		AddFormPanel.add(GroupNoSpinner);
+		
+		SubGroupNoSpinner = new JSpinner();
+		SubGroupNoSpinner.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 13));
+		SubGroupNoSpinner.setBounds(280, 295, 222, 29);
+		AddFormPanel.add(SubGroupNoSpinner);
+		
+		GrpID = new JTextField();
+		GrpID.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 13));
+		GrpID.setBounds(662, 168, 220, 29);
+		AddFormPanel.add(GrpID);
+		GrpID.setColumns(10);
+		
+		SubGrpID = new JTextField();
+		SubGrpID.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 13));
+		SubGrpID.setColumns(10);
+		SubGrpID.setBounds(662, 328, 220, 29);
+		AddFormPanel.add(SubGrpID);
+	
+	//clear button of add student group form
+		btnClearAdd = new JButton("Clear");
+		btnClearAdd.setForeground(Color.WHITE);
+		btnClearAdd.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 14));
+		btnClearAdd.setFocusPainted(false);
+		btnClearAdd.setBackground(new Color(31, 58, 147));
+		btnClearAdd.setBounds(529, 402, 132, 38);
+		AddFormPanel.add(btnClearAdd);
+	
+		
+	//clear button of add student group form	
+		btnSaveAdd = new JButton("Save");
+		btnSaveAdd.setForeground(Color.WHITE);
+		btnSaveAdd.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 14));
+		btnSaveAdd.setFocusPainted(false);
+		btnSaveAdd.setBackground(new Color(27, 163, 156));
+		btnSaveAdd.setBounds(719, 402, 132, 38);
+		AddFormPanel.add(btnSaveAdd);
+		
+	//Generate Group id button of add student group form
+		btnGenerateGID = new JButton("Generate GroupID");
+		btnGenerateGID.setForeground(Color.WHITE);
+		btnGenerateGID.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 14));
+		btnGenerateGID.setFocusPainted(false);
+		btnGenerateGID.setBackground(new Color(31, 58, 147));
+		btnGenerateGID.setBounds(662, 85, 220, 38);
+		AddFormPanel.add(btnGenerateGID);
+	
+		
+	//Generate Sub Group id button of add student group form
+		btnGenerateSubGID = new JButton("Generate Sub GroupID");
+		btnGenerateSubGID.setActionCommand("Generate Sub GroupID");
+		btnGenerateSubGID.setForeground(Color.WHITE);
+		btnGenerateSubGID.setFont(new Font("Leelawadee UI Semilight", Font.BOLD, 14));
+		btnGenerateSubGID.setFocusPainted(false);
+		btnGenerateSubGID.setBackground(new Color(31, 58, 147));
+		btnGenerateSubGID.setBounds(662, 248, 220, 38);
+		AddFormPanel.add(btnGenerateSubGID);
 		
 		
 		
